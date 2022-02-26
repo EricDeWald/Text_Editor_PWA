b09ee7e77787ad19f91215216a6851601bb894ab
EricDeWald <eric.l.dewald@gmail.com>
2022-02-26 13:53:30 -0600

    Updated webpack config.
    Added workbox plugins and service worker, manifest files, css loaders and babel.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,45 +3,35 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
-
-
 module.exports = () => {
   return {
     mode: 'development',
-    // Entry point for files
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js',
-      cards: './src/js/cards.js'
+      install: './src/js/install.js'
     },
-    // Output for our bundles
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      // Webpack plugin that generates our html file and injects our bundles. 
+      // This webpack plugin generates our html file and injects our bundles. 
       new HtmlWebpackPlugin({
         template: './index.html',
         title: 'Contact Cards'
       }),
-     
-      // Injects our custom service worker
+      // This injects our custom service worker
       new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
       }),
-
       // Creates a manifest.json file.
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Contact Cards',
-        short_name: 'Contact',
-        description: 'Never forget your contacts!',
+        name: 'Just another text editor',
+        short_name: 'JATE',
+        description: 'Note taker with use offline.',
         background_color: '#225ca3',
         theme_color: '#225ca3',
         start_url: '/',
@@ -66,7 +56,7 @@ module.exports = () => {
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
-          // We use babel-loader in order to use ES6.
+          // babel-loader to use ES6.
           use: {
             loader: 'babel-loader',
             options: {
